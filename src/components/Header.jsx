fc6f3bebca3cbd0bb6ac41d4b075930d3812d12b
Abdur Rahman <bmishu33@gmail.com>
2024-04-03 01:05:37 -0700

    Added homepage with animations

---
+++ src/components/Header.jsx
@@ -0,0 +1,48 @@
+import logo from "../assets/images/logoMemoji.png";
+export default function Header() {
+  return (
+    <header className="p-4 z-10">
+      <div className="container mx-auto flex justify-between items-center">
+        <div>
+          <img className="rounded-full w-16 h-16" src={logo} />
+        </div>
+        <div className="bg-white rounded-full p-1">
+          <ul className="list-none flex">
+            <li className="me-1">
+              <a href="#" className="block bg-gray-300 border border-transparent hover:bg-gray-400 px-5 py-2 rounded-full">
+                Myself
+              </a>
+            </li>
+            <li className="mx-1">
+              <a href="#" className="block border border-gray-300 hover:bg-gray-400 hover:border-transparent px-5 py-2 rounded-full">
+                Works
+              </a>
+            </li>
+            <li className="ms-1">
+              <a href="#" className="block border border-gray-300 hover:bg-gray-400 hover:border-transparent px-5 py-2 rounded-full">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <button className="bg-white w-16 h-16 rounded-full flex justify-center items-center">
+            <svg
+              width="24px"
+              height="24px"
+              strokeWidth={1.5}
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              color="#000000"
+            >
+              <path d="M3 5H11" stroke="#000000" strokeWidth={1.5} strokeLinecap="round" strokeLinejoin="round" />
+              <path d="M3 12H16" stroke="#000000" strokeWidth={1.5} strokeLinecap="round" strokeLinejoin="round" />
+              <path d="M3 19H21" stroke="#000000" strokeWidth={1.5} strokeLinecap="round" strokeLinejoin="round" />
+            </svg>
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+}
---
+++ src/components/NameFlow.jsx
@@ -0,0 +1,18 @@
+export default function NameFlow() {
+  return (
+    <div className="name-flow-wrapper">
+      <div className="name-flow-inner">
+        <div className="name-flow glitch-wrapper">
+          <h1 className="name-flow-text glitch" data-text="Abdur Rahman (Mishu)">
+            Abdur Rahman (Mishu)<span className="spacer">—</span>
+          </h1>
+        </div>
+        <div className="name-flow glitch-wrapper">
+          <h1 className="name-flow-text glitch" data-text="Abdur Rahman (Mishu)">
+            Abdur Rahman (Mishu)<span className="spacer">—</span>
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+}
